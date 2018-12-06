5ce87769714e0f4d7e571b7fc8f575f11dff1a6d
zhangjixiang2018 <44158103+zhangjixiang2018@users.noreply.github.com>
2018-12-06 21:01:17 +0800

    Buffered

---
+++ src/io/BufferedReaderTest.java
@@ -0,0 +1,29 @@
+package io;
+
+import java.io.*;
+import java.util.Random;
+
+public class BufferedReaderTest {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:/BufferedReaderTest.txt");
+        if(!file.exists()){
+            file.createNewFile();
+        }
+
+        BufferedReader br = new BufferedReader(new FileReader(file));//创建BufferedReader
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+
+        String s ;
+        for(int i=0 ; i<100 ; i++){//产生随机数，并将随机数转化为字符串，最后将这个字符串写入文件
+            s = String.valueOf(Math.random());
+            bw.write(s);
+            bw.newLine();
+        }
+        bw.close();
+
+        while((s = br.readLine())!= null){
+            System.out.println(s);
+        }
+        br.close();
+    }
+}
